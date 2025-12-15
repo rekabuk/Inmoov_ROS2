a4d9aeeab6bebfc9b1be6046552dbd4216654c6f
rekabuk <andrew@rekabuk.co.uk>
2025-12-15 08:13:13 -0500

    Restart - Not sure how to commit/push

--- inmoov_ws/xicro_build.sh
+++ inmoov_ws/xicro_build.sh
@@ -29,10 +29,10 @@ ros2 run xicro_pkg generate_xicro_node.py -mcu_type arduino
 
 ####################################################################
 # Subsystem 3 (Head)
-#cp src/Xicro/xicro_pkg/config/setup_xicro_subsystem3.yaml src/Xicro/xicro_pkg/config/setup_xicro.yaml
-#colcon build 
+cp src/Xicro/xicro_pkg/config/setup_xicro_subsystem3.yaml src/Xicro/xicro_pkg/config/setup_xicro.yaml
+colcon build 
 #source install/setup.bash
 
-#ros2 run xicro_pkg generate_library.py -mcu_type arduino
+ros2 run xicro_pkg generate_library.py -mcu_type arduino
 
-#ros2 run xicro_pkg generate_xicro_node.py -mcu_type arduino
+ros2 run xicro_pkg generate_xicro_node.py -mcu_type arduino
