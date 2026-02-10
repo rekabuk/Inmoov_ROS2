2e4cac5fa2e6e984679306ac9b975c6ad34f5e11
rekabuk <andrew@rekabuk.co.uk>
2026-02-10 15:47:13 -0500

    Subscribe to many topics and callback with index

--- inmoov_ws/src/inmoov_capture/inmoov_capture/inmoov_capture_multi.py
+++ inmoov_ws/src/inmoov_capture/inmoov_capture/inmoov_capture_multi.py
@@ -1,20 +1,22 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from std_msgs.msg import Int16
 from functools import partial
+
 
 class MultiTopicSubscriber(Node):
     def __init__(self):
         super().__init__('multi_topic_subscriber')
         
         # Array of topics to subscribe to
-        self.topic_names = ['/topic1', '/topic2', '/topic3']
+        self.topic_names = ['/shoulder_R', '/rotate_R', '/omoplate_R','/jaw']
         self.subs = []
         
         # Create a subscriber for each topic
         for topic in self.topic_names:
             sub = self.create_subscription(
-                String,
+                Int16,
                 topic,
                 partial(self.listener_callback, index=1),
                 10)
@@ -23,7 +25,7 @@ class MultiTopicSubscriber(Node):
 
     def listener_callback(self, msg, index):
         # Callback processes messages from all topics
-        self.get_logger().info(f'Received: "{msg.data}"')
+        self.get_logger().info(f'Received: "{self.topic_names[index]}" "{msg.data}"')
 
 def main(args=None):
     rclpy.init(args=args)
